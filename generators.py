535a79053c6301663f6abf70cd9160c5c132d563
Liberbell <liberbell@gmail.com>
2022-01-06 08:58:41 +0900

    generators5

--- generators.py
+++ generators.py
@@ -30,5 +30,20 @@ def myGenerator():
 gen = myGenerator()
 
 print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+def generate_sequence():
+    n = 1
+    yield 10
+
+    n = n + 2
+    yield 20
+
+    n = n + 3
+    yield 30
+
+gen = generate_sequence()
+print(next(gen))
 print(next(gen))
 print(next(gen))
