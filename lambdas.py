46b906db62adec9816c19cefd4565cefda228f7e
Liberbell <liberbell@gmail.com>
2022-01-05 09:35:26 +0900

    lambdas1

--- lambdas.py
+++ lambdas.py
@@ -6,3 +6,15 @@ increment_fn = lambda x : x + 1
 print(increment_fn)
 print(increment_fn(10))
 # print(increment_fn(10, 2))
+
+def square(number):
+    return number ** 2
+
+def cube(number):
+    return number ** 3
+
+def square_root(number):
+    return number ** (1/2)
+
+def cube_root(number):
+    return number ** (1/3)
