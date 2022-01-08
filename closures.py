d7c6ad65f25d37bbc1a82e6912bfcbf70384b007
Liberbell <liberbell@gmail.com>
2022-01-08 10:25:39 +0900

    closures11

--- closures.py
+++ closures.py
@@ -41,3 +41,6 @@ def get_favorite_programming_language(favorite_language):
 
 display_function_python = get_favorite_programming_language("Python")
 display_function_python()
+
+display_function_python = get_favorite_programming_language("Java")
+display_function_python()
