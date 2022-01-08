4fe0bbf96a54fc17bd98eff876bb2ea4fc42dae9
Liberbell <liberbell@gmail.com>
2022-01-08 10:14:39 +0900

    closures2

--- closures.py
+++ closures.py
@@ -8,3 +8,12 @@ def outer_fn():
 # print(outer_fn())
 # outer_fn()
 # display_favorite_programming_language()
+
+def get_favorite_programming_language():
+    def display_favorite_programming_language():
+        print("My favorite programming language is Python.")
+
+    print("Executing the outer function")
+    return display_favorite_programming_language()
+
+display_fn = get_favorite_programming_language()
