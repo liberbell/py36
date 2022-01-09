896ed937e9e34f418a4997382e41a19a04fcb82f
Liberbell <liberbell@gmail.com>
2022-01-09 09:46:49 +0900

    decorators2

--- decorators.py
+++ decorators.py
@@ -2,4 +2,8 @@
 def compute_rectangle_area(lenght, breadth):
     return lenght * breadth
 
-def non_negative_argument(decorate_fn):
+def non_negative_argument(decorated_fn):
+    def check_non_negative(*args):
+        for arg in args:
+            if arg <= 0:
+                raise ValueError("")
