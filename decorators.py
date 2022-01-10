199b2a410ffa04c3886afebdfc9f991e4e36a292
Liberbell <liberbell@gmail.com>
2022-01-10 18:05:08 +0900

    decorators11

--- decorators.py
+++ decorators.py
@@ -63,5 +63,5 @@ def display_entertaining_message():
     print("This is an entertaining message.")
 
 display_important_message()
-# display_boring_message()
-# display_entertaining_message()
+display_boring_message()
+display_entertaining_message()
